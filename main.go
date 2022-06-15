ad68c6620183ac0aa49d1da8f09620f8f2c9fead
Admin <11172909+demo_42_0@user.noreply.gitee.com>
2022-06-15 11:11:36 +0800

    second commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"demo/db"
+	"demo/service"
+	"fmt"
+	"log"
+	"net/http"
+
+	"os"
+)
+
+func main() {
+	if err := db.InitMysql(); err != nil {
+		panic(fmt.Sprintf("mysql init failed with %+v", err))
+	}
+
+	if err := db.InitRedis(); err != nil {
+		panic(fmt.Sprintf("redis init failed with %+v", err))
+	}
+
+	if err := db.InitMongoDB(); err != nil {
+		panic(fmt.Sprintf("mongodb init failed with %+v", err))
+	}
+
+	http.HandleFunc("/", service.IndexHandler)
+	http.HandleFunc("/api/count", service.CounterHandler)
+
+
+    http.HandleFunc("/api/test", service.TestHandler)
+	http.HandleFunc("/api/get_follow_list", service.FollowListHandler)
+	http.HandleFunc("/api/get_follow_list_test", service.TestFollowListHandler)
+	http.HandleFunc("/v1/ping", service.PingHandler)
+
+	listenPort := os.Getenv("_FAAS_RUNTIME_PORT")
+	if listenPort == "" {
+		log.Fatal("failed to load _FAAS_RUNTIME_PORT")
+	}
+	fmt.Println("http ListenAndServe ", listenPort)
+	log.Fatal(http.ListenAndServe(listenPort, nil))
+}
