94bb891b814865c559044a85855161c12d61e3c4
张宇 <zhangyu.me@bytedance.com>
2022-06-16 15:35:12 +0800

    feat: fix

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
-set -ex
-cd `dirname $0`
+#!/bin/sh
+#set -ex
+#cd `dirname $0`
 
-if [ -d "output" ]; then
-    exec ./output/demo
-else
-    exec ./demo
-fi
+#if [ -d "output" ]; then
+#    exec ./output/demo
+#else
+#    exec ./demo
+#fi
+
+cd /opt/application/ && ./main
